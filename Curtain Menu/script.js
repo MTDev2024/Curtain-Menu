c12a7f9f3a70f70f04969f0c970ce4f3d68d7ab8
M <145024052+MTDev2024@users.noreply.github.com>
2024-03-20 15:15:37 +0100

    Add files via upload

---
+++ Curtain Menu/script.js
@@ -0,0 +1,17 @@
+const hamburgerButton = document.querySelector
+(".nav-toggler")
+
+const navigation = document.querySelector
+("nav")
+
+hamburgerButton.addEventListener
+("click", toggleNav)
+
+function toggleNav(){
+    hamburgerButton.classList.toggle("active")
+}
+
+function toggleNav(){
+    hamburgerButton.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
